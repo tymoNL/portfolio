6391064c1d51d23d014568f9a4cc87cedd290884
Tymo <tymo.smids@gmail.com>
2025-12-08 15:14:23 +0100

    0.2

--- js/script.js
+++ js/script.js
@@ -2,4 +2,18 @@ function ToggleColorMode(obj) {
     const body = document.body;
     body.classList.toggle('lightMode');
     body.classList.toggle('darkMode');
+
+    obj.querySelector('i').classList.toggle('fa-moon');
+    obj.querySelector('i').classList.toggle('fa-sun');
 }
+
+function BodyScrolled() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop > 0) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('scroll', BodyScrolled);
